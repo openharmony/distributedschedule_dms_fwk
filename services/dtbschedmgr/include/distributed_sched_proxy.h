71d2763e9f6e924ab032bede1e1b30ef0bcc81af
qianli <847171309@qq.com>
2021-06-17 11:34:15 +0800

    add dms
    Signed-off-by: qianli <847171309@qq.com>
    Change-Id: I959868ca8996446b66a801e6875b75b764e5b234

---
+++ services/dtbschedmgr/include/distributed_sched_proxy.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef HARMONYOS_DISTRIBUTED_SCHED_PROXY_H
+#define HARMONYOS_DISTRIBUTED_SCHED_PROXY_H
+
+#include "distributed_sched_interface.h"
+#include "iremote_proxy.h"
+
+namespace OHOS {
+namespace DistributedSchedule {
+class DistributedSchedProxy : public IRemoteProxy<IDistributedSched>{
+public:
+    explicit DistributedSchedProxy(const sptr<IRemoteObject>& impl)
+        : IRemoteProxy<IDistributedSched>(impl) {}
+    ~DistributedSchedProxy(){}
+    int32_t StartRemoteAbility(const OHOS::AAFwk::Want& userWant, OHOS::AAFwk::Want& innerWant, int32_t requestCode) override;
+    int32_t StartAbilityFromRemote(const OHOS::AAFwk::Want& userWant, OHOS::AAFwk::Want& innerWant, int32_t requestCode) override;
+
+private:
+    static inline BrokerDelegator<DistributedSchedProxy> delegator_;
+};
+
+} // namespace DistributedSchedule
+} // namespace OHOS
+
+#endif // HARMONYOS_DISTRIBUTED_SCHED_PROXY_H
---
+++ services/dtbschedmgr/src/distributed_sched_proxy.cpp
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "distributed_sched_proxy.h"
+#include "dtbschedmgr_log.h"
+#include "ipc_types.h"
+#include "parcel_helper.h"
+#include "string_ex.h"
+
+using namespace std;
+using namespace OHOS::AAFwk;
+
+namespace OHOS {
+namespace DistributedSchedule {
+namespace {
+const std::u16string DMS_PROXY_INTERFACE_TOKEN = u"ohos.distributedschedule.accessToken";
+}
+int32_t DistributedSchedProxy::StartRemoteAbility(const OHOS::AAFwk::Want& userWant,
+    OHOS::AAFwk::Want& innerWant, int32_t requestCode)
+{
+    sptr<IRemoteObject> remote = Remote();
+    if (remote == nullptr){
+        return ERR_NULL_OBJECT;
+    }
+    MessageParcel data;
+    if(!data.WriteInterfaceToken(DMS_PROXY_INTERFACE_TOKEN)) {
+        return ERR_FLATTEN_OBJECT;
+    }
+    PARCEL_WRITE_HELPER(data, Parcelable, &userWant);
+    PARCEL_WRITE_HELPER(data, Parcelable, &innerWant);
+    PARCEL_WRITE_HELPER(data, Int32, requestCode);
+    MessageParcel reply;
+    PARCEL_TRANSACT_SYNC_RET_INT(remote,START_REMOTE_ABILITY,data,reply);
+}
+
+int32_t DistributedSchedProxy::StartAbilityFromRemote(const OHOS::AAFwk::Want& userWant,
+    OHOS::AAFwk::Want& innerWant,int32_t requestCode)
+{
+        sptr<IRemoteObject> remote = Remote();
+    if (remote == nullptr){
+        return ERR_NULL_OBJECT;
+    }
+    MessageParcel data;
+    if(!data.WriteInterfaceToken(DMS_PROXY_INTERFACE_TOKEN)) {
+        return ERR_FLATTEN_OBJECT;
+    }
+    PARCEL_WRITE_HELPER(data, Parcelable, &userWant);
+    PARCEL_WRITE_HELPER(data, Parcelable, &innerWant);
+    PARCEL_WRITE_HELPER(data, Int32, requestCode);
+    MessageParcel reply;
+    PARCEL_TRANSACT_SYNC_RET_INT(remote,START_ABILITY_FROM_REMOTE,data,reply);
+}
+} // namespace DistributedSchedule
+} // namespace OHOS
+
