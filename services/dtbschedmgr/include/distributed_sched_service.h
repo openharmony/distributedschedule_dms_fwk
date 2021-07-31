8323f73aa1e3fbf3bc4482f23765ae35a9fab3bb
ql <qianli16@huawei.com>
2021-07-31 02:36:49 +0000

    update services/dtbschedmgr/include/distributed_sched_service.h. Signed-off-by: NEEN <qianli16@huawei.com>

--- services/dtbschedmgr/include/distributed_sched_service.h
+++ services/dtbschedmgr/include/distributed_sched_service.h
@@ -49,7 +49,8 @@ public:
     ~DistributedSchedService() = default;
     void OnStart() override;
     void OnStop() override;
-    int32_t StartRemoteAbility(const OHOS::AAFwk::Want& userWant, OHOS::AAFwk::Want& innerWant, int32_t requestCode) override;
+    int32_t StartRemoteAbility(const OHOS::AAFwk::Want& userWant, OHOS::AAFwk::Want& innerWant, 
+        int32_t requestCode) override;
     int32_t StartAbilityFromRemote(const OHOS::AAFwk::Want& userWant, OHOS::AAFwk::Want& innerWant, 
         int32_t requestCode) override;
 private:
