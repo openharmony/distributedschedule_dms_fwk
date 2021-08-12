00c0d62afdf34448916ec399081f2db6b9dbac16
ql <qianli16@huawei.com>
2021-08-12 07:22:43 +0000

    update services/dtbschedmgr/include/distributed_sched_interface.h. Signed-off-by: NEEN <qianli16@huawei.com>

--- services/dtbschedmgr/include/distributed_sched_interface.h
+++ services/dtbschedmgr/include/distributed_sched_interface.h
@@ -46,8 +46,8 @@ public:
         std::string callerAppId;
         std::vector<std::string> bundleNames;
     };
-    virtual int32_t StartRemoteAbility(const OHOS::AAFwk::Want& userWant, const OHOS::AppExecFwk::AbilityInfo& abilityInfo,
-        int32_t requestCode) = 0;
+    virtual int32_t StartRemoteAbility(const OHOS::AAFwk::Want& userWant,
+        const OHOS::AppExecFwk::AbilityInfo& abilityInfo, int32_t requestCode) = 0;
     virtual int32_t StartAbilityFromRemote(const OHOS::AAFwk::Want& userWant,
         const OHOS::AppExecFwk::AbilityInfo& abilityInfo, int32_t requestCode, const CallerInfo& callerInfo,
         const AccountInfo& accountInfo) = 0;
