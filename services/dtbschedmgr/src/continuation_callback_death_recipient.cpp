2377847b3ce7d7053a705b669ed30ccd94be5368
wangdongdong <wangdd_zju@126.com>
2021-09-09 18:17:48 +0800

    add continuation implementation
    Signed-off-by: wangdongdong <wangdd_zju@126.com>
    Change-Id: I7253fb92d87e94749dd539ed30708eff5bec44cb

---
+++ services/dtbschedmgr/src/continuation_callback_death_recipient.cpp
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "continuation_callback_death_recipient.h"
+#include "distributed_sched_ability_shell.h"
+#include "dtbschedmgr_log.h"
+#include "iremote_proxy.h"
+
+namespace OHOS {
+namespace DistributedSchedule {
+void ContinuationCallbackDeathRecipient::OnRemoteDied(const wptr<IRemoteObject>& remote)
+{
+    HILOGI("%s called", __func__);
+    DistributedSchedAbilityShell::GetInstance().RemoveContinuationCallback(remote.promote());
+}
+} // namespace DistributedSchedule
+} // namespace OHOS
