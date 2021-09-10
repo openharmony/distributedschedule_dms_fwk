0f6b08caff18ba1caed41673ea8f6a0b46317eb3
Gymee <yumeijie@huawei.com>
2021-09-10 10:36:40 +0800

    add connect ability
    Signed-off-by: Gymee <yumeijie@huawei.com>
    Change-Id: I301b305fd200cf3c37112f190aba35f482f8800e

---
+++ services/dtbschedmgr/src/ability_connection_wrapper_proxy.cpp
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "ability_connection_wrapper_proxy.h"
+
+#include "dtbschedmgr_log.h"
+#include "ipc_object_proxy.h"
+#include "ipc_types.h"
+#include "parcel_helper.h"
+
+namespace OHOS {
+namespace DistributedSchedule {
+void AbilityConnectionWrapperProxy::OnAbilityConnectDone(const AppExecFwk::ElementName& element,
+    const sptr<IRemoteObject>& remoteObject, int32_t resultCode)
+{
+    HILOGD("AbilityConnectionWrapperProxy::OnAbilityConnectDone called");
+    MessageParcel data;
+    if (!data.WriteInterfaceToken(IAbilityConnection::GetDescriptor())) {
+        return;
+    }
+
+    MessageParcel reply;
+    MessageOption option;
+    PARCEL_WRITE_HELPER_NORET(data, Parcelable, &element);
+    PARCEL_WRITE_HELPER_NORET(data, RemoteObject, remoteObject);
+    PARCEL_WRITE_HELPER_NORET(data, Int32, resultCode);
+    HILOGD("ConnectAbilityFromRemote Remote() is %{public}p", Remote().GetRefPtr());
+    int32_t errCode = Remote()->SendRequest(IAbilityConnection::ON_ABILITY_CONNECT_DONE, data, reply, option);
+    HILOGD("AbilityConnectionWrapperProxy::OnAbilityConnectDone result %{public}d", errCode);
+}
+
+void AbilityConnectionWrapperProxy::OnAbilityDisconnectDone(const AppExecFwk::ElementName& element, int32_t resultCode)
+{
+    HILOGD("AbilityConnectionWrapperProxy::OnAbilityDisconnectDone called");
+    MessageParcel data;
+    if (!data.WriteInterfaceToken(IAbilityConnection::GetDescriptor())) {
+        return;
+    }
+
+    MessageParcel reply;
+    MessageOption option;
+    PARCEL_WRITE_HELPER_NORET(data, Parcelable, &element);
+    PARCEL_WRITE_HELPER_NORET(data, Int32, resultCode);
+    int32_t errCode = Remote()->SendRequest(IAbilityConnection::ON_ABILITY_DISCONNECT_DONE, data, reply, option);
+    HILOGD("AbilityConnectionWrapperProxy::OnAbilityDisconnectDone result %{public}d", errCode);
+}
+} // namespace DistributedSchedule
+} // namespace OHOS
---
+++ services/dtbschedmgr/src/ability_connection_wrapper_stub.cpp
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "ability_connection_wrapper_stub.h"
+
+#include "ability_connection_wrapper_proxy.h"
+#include "dtbschedmgr_log.h"
+#include "ipc_types.h"
+
+namespace OHOS {
+namespace DistributedSchedule {
+using namespace AAFwk;
+
+AbilityConnectionWrapperStub::AbilityConnectionWrapperStub(sptr<IRemoteObject> connection)
+{
+    distributedConnection_ = connection;
+}
+
+int32_t AbilityConnectionWrapperStub::OnRemoteRequest(uint32_t code, MessageParcel& data,
+    MessageParcel& reply, MessageOption& option)
+{
+    HILOGD("AbilityConnectionWrapperStub::OnRemoteRequest code = %{public}d", code);
+    std::u16string descriptor = IAbilityConnection::GetDescriptor();
+    std::u16string remoteDescriptor = data.ReadInterfaceToken();
+    if (descriptor != remoteDescriptor) {
+        HILOGE("AbilityConnectionWrapperStub local descriptor is not equal to remote");
+        return ERR_INVALID_STATE;
+    }
+
+    sptr<AppExecFwk::ElementName> element(data.ReadParcelable<AppExecFwk::ElementName>());
+    if (element == nullptr) {
+        HILOGE("AbilityConnectionWrapperStub element is null");
+        return ERR_INVALID_VALUE;
+    }
+    switch (code) {
+        case IAbilityConnection::ON_ABILITY_CONNECT_DONE: {
+            if (auto remoteObject = data.ReadParcelable<IRemoteObject>()) {
+                auto resultCode = data.ReadInt32();
+                OnAbilityConnectDone(*element, remoteObject, resultCode);
+                return ERR_NONE;
+            }
+            HILOGE("AbilityConnectionWrapperStub remoteObject is null");
+            return ERR_INVALID_DATA;
+        }
+        case IAbilityConnection::ON_ABILITY_DISCONNECT_DONE: {
+            auto resultCode = data.ReadInt32();
+            OnAbilityDisconnectDone(*element, resultCode);
+            return ERR_NONE;
+        }
+        default: {
+            HILOGE("AbilityConnectionWrapperStub unknown code");
+            return IPCObjectStub::OnRemoteRequest(code, data, reply, option);
+        }
+    }
+}
+
+void AbilityConnectionWrapperStub::OnAbilityConnectDone(const AppExecFwk::ElementName& element,
+    const sptr<IRemoteObject>& remoteObject, int32_t resultCode)
+{
+    auto proxy = std::make_unique<AbilityConnectionWrapperProxy>(distributedConnection_);
+    proxy->OnAbilityConnectDone(element, remoteObject, resultCode);
+}
+
+void AbilityConnectionWrapperStub::OnAbilityDisconnectDone(const AppExecFwk::ElementName& element,
+    int32_t resultCode)
+{
+    auto proxy = std::make_unique<AbilityConnectionWrapperProxy>(distributedConnection_);
+    proxy->OnAbilityDisconnectDone(element, resultCode);
+}
+} // namespace DistributedSchedule
+} // namespace OHOS
