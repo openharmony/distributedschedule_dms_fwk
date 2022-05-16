5868f9e202081dc159c20b93a89b41c35b5b0769
wangyang2022 <wangyang412@huawei.com>
2022-05-16 20:41:38 +0800

    const 修饰入参 Signed-off-by: wangyang2022 <wangyang412@huawei.com> Change-Id: I5cc41ae48a886f3018ce83b59c79d52e850ba97c

--- services/dtbschedmgr/include/bundle/bundle_manager_internal.h
+++ services/dtbschedmgr/include/bundle/bundle_manager_internal.h
@@ -34,7 +34,7 @@ public:
     static bool GetBundleNameListFromBms(int32_t callingUid, std::vector<std::u16string>& u16BundleNameList);
     static bool QueryAbilityInfo(const AAFwk::Want& want, AppExecFwk::AbilityInfo& abilityInfo);
     static bool QueryExtensionAbilityInfo(const AAFwk::Want& want, AppExecFwk::ExtensionAbilityInfo& extensionInfo);
-    static void InitAbilityInfoFromExtension(AppExecFwk::ExtensionAbilityInfo &extensionAbilityInfo,
+    static void InitAbilityInfoFromExtension(const AppExecFwk::ExtensionAbilityInfo &extensionAbilityInfo,
         AppExecFwk::AbilityInfo &abilityInfo);
     static bool IsSameAppId(const std::string& callerAppId, const std::string& targetBundleName);
     static int32_t GetLocalBundleInfo(const std::string& bundleName, AppExecFwk::BundleInfo &localBundleInfo);
