0dcbce2ed25f099345605ba32eddd400ebacc1a9
wangyang2022 <wangyang412@huawei.com>
2022-05-16 16:19:47 +0800

    修改单元测试 Signed-off-by: wangyang2022 <wangyang412@huawei.com> Change-Id: Ibb5830578868b770ff160a3875ab8a27e05b1b40

--- services/dtbschedmgr/src/distributed_sched_permission.cpp
+++ services/dtbschedmgr/src/distributed_sched_permission.cpp
@@ -172,7 +172,8 @@ bool DistributedSchedPermission::GetTargetAbility(const AAFwk::Want& want,
             return true;
         }
     }
-    HILOGE("QueryAbilityInfo failed, want bundle name=%{public}s.", want.GetBundle().c_str());
+    HILOGE("QueryAbilityInfo failed, want bundle name=%{public}s, ability name=%{public}s.",
+        want.GetElement().GetBundleName.c_str(), want.GetElement().GetAbilityName().c_str());
     return false;
 }
 
