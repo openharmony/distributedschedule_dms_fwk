741535b0f07a47bd73637fa5828173af7b845e36
majiajun <majiajun@huawei.com>
2022-06-23 01:13:29 -0700

    distributedsche process memory optimization Signed-off-by: majiajun <majiajun@huawei.com>
    Change-Id: I7112242a7124d7fc352165c168a3b26dcc8d9ab6

---
+++ services/dtbabilitymgr/include/distributed_ability_manager_interface.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef OHOS_DISTRIBUTED_ABILITY_MANAGER_INTERFACE_H
+#define OHOS_DISTRIBUTED_ABILITY_MANAGER_INTERFACE_H
+
+#include "iremote_broker.h"
+
+namespace OHOS {
+namespace DistributedSchedule {
+class IDistributedAbilityManager : public OHOS::IRemoteBroker {
+public:
+    DECLARE_INTERFACE_DESCRIPTOR(u"OHOS.DistributedSchedule.IDistributedAbilityManager");
+    virtual int32_t SendRequestToImpl(uint32_t code, MessageParcel& data, MessageParcel& reply,
+        MessageOption& option) = 0;
+};
+} // namespace DistributedSchedule
+} // namespace OHOS
+#endif // OHOS_DISTRIBUTED_ABILITY_MANAGER_INTERFACE_H
